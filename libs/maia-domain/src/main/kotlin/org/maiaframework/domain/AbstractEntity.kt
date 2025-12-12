aa077269e7797e492a429499ca5de439cec6645c
kevins-mahana <kevin.stembridge@mahanaroad.com>
2025-12-12 08:26:56 +0000

    Deleting obsolete classes in AbstractEntity hierarchy

--- libs/maia-domain/src/main/kotlin/org/maiaframework/domain/AbstractEntity.kt
+++
@@ -1,9 +0,0 @@
-package org.maiaframework.domain
-
-import java.time.Instant
-
-@Deprecated("The functionality provided by this class is now rendered directly into the generated DAOs")
-abstract class AbstractEntity protected constructor(
-    val createdTimestampUtc: Instant,
-    val id: DomainId
-)
