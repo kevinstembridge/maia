ecd38902c30d5cf35856658fac7573b1b16a78a9
kevins-mahana <kevin.stembridge@mahanaroad.com>
2026-01-27 23:14:46 +0000

    WIP on composite PK

--- libs/maia-toggles-parent/maia-toggles-showcase/src/main/kotlin/org/maiaframework/toggles/sample/SampleFeatureOne.kt
+++ libs/maia-toggles-parent/maia-toggles-showcase/src/main/kotlin/org/maiaframework/toggles/sample/SampleFeatureOne.kt
@@ -3,6 +3,7 @@ package org.maiaframework.toggles.sample
 import org.maiaframework.toggles.Feature
 import org.maiaframework.toggles.fields.ContactPerson
 
+
 object SampleFeatureOne : Feature(
     contactPerson = ContactPerson("Muriel")
 )
--- libs/maia-toggles-parent/maia-toggles-showcase/src/main/kotlin/org/maiaframework/toggles/sample/SampleFeatureTwo.kt
+++ libs/maia-toggles-parent/maia-toggles-showcase/src/main/kotlin/org/maiaframework/toggles/sample/SampleFeatureTwo.kt
@@ -3,6 +3,7 @@ package org.maiaframework.toggles.sample
 import org.maiaframework.toggles.Feature
 import org.maiaframework.toggles.fields.ContactPerson
 
+
 object SampleFeatureTwo : Feature(
     contactPerson = ContactPerson("Muriel"),
     enabledByDefault = true
