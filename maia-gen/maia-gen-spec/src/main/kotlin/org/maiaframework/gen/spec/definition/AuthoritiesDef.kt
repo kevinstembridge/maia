e71dba885d35e476af304b956bd5cf77212c5069
kevins-mahana <kevin.stembridge@mahanaroad.com>
2026-02-23 15:59:24 +0000

    WIP on Maia showcase UI

--- maia-gen/maia-gen-spec/src/main/kotlin/org/maiaframework/gen/spec/definition/AuthoritiesDef.kt
+++ maia-gen/maia-gen-spec/src/main/kotlin/org/maiaframework/gen/spec/definition/AuthoritiesDef.kt
@@ -7,7 +7,16 @@ data class AuthoritiesDef(val enumDef: EnumDef) {
     val importStatement = "import { ${enumDef.uqcn} } from '@${GeneratedTypescriptDir.forPackage(enumDef.fqcn.packageName)}/${enumDef.uqcn}';"
 
 
-    val authGuardRenderedFilePath = "${GeneratedTypescriptDir.forPackage(enumDef.fqcn.packageName)}/${enumDef.uqcn}.ts"
+    val authGuardRenderedFilePath = "${GeneratedTypescriptDir.forPackage(enumDef.fqcn.packageName)}/auth.guard.ts"
+
+
+    val authServiceRenderedFilePath = "${GeneratedTypescriptDir.forPackage(enumDef.fqcn.packageName)}/auth.service.ts"
+
+
+    val authApiServiceRenderedFilePath = "${GeneratedTypescriptDir.forPackage(enumDef.fqcn.packageName)}/auth-api.service.ts"
+
+
+    val userSummaryDtoRenderedFilePath = "${GeneratedTypescriptDir.forPackage(enumDef.fqcn.packageName)}/UserSummaryDto.ts"
 
 
 }
