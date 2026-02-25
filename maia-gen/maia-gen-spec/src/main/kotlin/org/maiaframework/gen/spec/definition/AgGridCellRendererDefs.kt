a6afe98a73c8440e62288a580453fd2beb18e415
kevins-mahana <kevin.stembridge@mahanaroad.com>
2026-02-25 23:37:28 +0000

    WIP on Angular upgrade

--- maia-gen/maia-gen-spec/src/main/kotlin/org/maiaframework/gen/spec/definition/AgGridCellRendererDefs.kt
+++ maia-gen/maia-gen-spec/src/main/kotlin/org/maiaframework/gen/spec/definition/AgGridCellRendererDefs.kt
@@ -4,13 +4,13 @@ object AgGridCellRendererDefs {
 
 
     val deleteAction = AgGridCellRendererDef(
-        "import {IconAgGridCellRendererComponent} from '@app/components/icon-ag-grid-cell-renderer/icon-ag-grid-cell-renderer.component';",
+        "import {IconAgGridCellRendererComponent} from '@maia/maia-ui';",
         "IconAgGridCellRendererComponent"
     )
 
 
     val editAction = AgGridCellRendererDef(
-        "import {IconAgGridCellRendererComponent} from '@app/components/icon-ag-grid-cell-renderer/icon-ag-grid-cell-renderer.component';",
+        "import {IconAgGridCellRendererComponent} from '@maia/maia-ui';",
         "IconAgGridCellRendererComponent"
     )
 
