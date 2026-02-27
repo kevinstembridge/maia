e7397b159b4db4622b7ea7db3a45d0b780b35103
kevins-mahana <kevin.stembridge@mahanaroad.com>
2026-02-27 23:22:37 +0000

    WIP on Maia Showcase UI

--- maia-showcase/maia-showcase-ui/src/app/app.routes.ts
+++ maia-showcase/maia-showcase-ui/src/app/app.routes.ts
@@ -4,6 +4,13 @@ export const routes: Routes = [
     {
         path: '',
         loadComponent: () =>
+            import('@app/pages/home/home-page.component').then(
+                (m) => m.HomePageComponent,
+            ),
+    },
+    {
+        path: 'simple',
+        loadComponent: () =>
             import('@app/pages/simple/simple-page.component').then(
                 (m) => m.SimplePageComponent,
             ),
