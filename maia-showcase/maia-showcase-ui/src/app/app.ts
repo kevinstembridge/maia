f0a910939dbc7bbbebb442c1f63a28d55453fd5f
kevins-mahana <kevin.stembridge@mahanaroad.com>
2026-02-27 23:30:33 +0000

    WIP on Maia Showcase UI

--- maia-showcase/maia-showcase-ui/src/app/app.ts
+++ maia-showcase/maia-showcase-ui/src/app/app.ts
@@ -3,10 +3,11 @@ import {RouterLink, RouterOutlet} from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatMenu, MatMenuItem, MatMenuTrigger} from '@angular/material/menu';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, RouterLink, MatToolbarModule, MatButtonModule, MatIconModule],
+    imports: [RouterOutlet, RouterLink, MatToolbarModule, MatButtonModule, MatIconModule, MatMenu, MatMenuItem, MatMenuTrigger],
     templateUrl: './app.html',
     styleUrl: './app.css',
 })
