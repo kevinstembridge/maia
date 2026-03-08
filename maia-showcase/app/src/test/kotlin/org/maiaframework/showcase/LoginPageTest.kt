2d27cd7e53942f6f62d5233b3d28f1b09d2ad1a8
kevins-mahana <kevin.stembridge@mahanaroad.com>
2026-03-08 16:35:21 +0000

    WIP on Maia Showcase UI

---
+++ maia-showcase/app/src/test/kotlin/org/maiaframework/showcase/LoginPageTest.kt
@@ -0,0 +1,40 @@
+package org.maiaframework.showcase
+
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.maiaframework.showcase.testing.fixtures.UserFixture
+
+
+class LoginPageTest : AbstractPlaywrightTest() {
+
+
+    private lateinit var user: UserFixture
+
+
+    @BeforeAll
+    fun setUp() {
+
+        user = fixtures.aUser()
+        fixtures.resetDatabaseState()
+
+    }
+
+
+    @Test
+    fun `user can navigate to the login page and submit the form`() {
+
+        loginPage.navigateToMe()
+
+        Thread.sleep(300)
+
+        loginPage.submitForm(
+            user.userEntity.emailAddress,
+            user.rawPassword
+        )
+
+        landingPage.assertOnPage()
+
+    }
+
+
+}
