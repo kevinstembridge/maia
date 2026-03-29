31ded8cace8216fdc1b6b4fe601f8aa4a58b0eeb
kevins-mahana <kevin.stembridge@mahanaroad.com>
2026-03-29 21:09:34 +0100

    WIP on Maia Showcase UI

--- maia-showcase/app/src/test/kotlin/org/maiaframework/showcase/all_field_types/AllFieldTypesCrudPlaywrightTest.kt
+++ maia-showcase/app/src/test/kotlin/org/maiaframework/showcase/all_field_types/AllFieldTypesCrudPlaywrightTest.kt
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.maiaframework.showcase.AbstractPlaywrightTest
 
+
 class AllFieldTypesCrudPlaywrightTest : AbstractPlaywrightTest() {
 
 
