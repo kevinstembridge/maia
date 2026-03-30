59734e0ff7b836e9094546f526d78b511f7fca16
kevins-mahana <kevin.stembridge@mahanaroad.com>
2026-03-30 07:49:17 +0100

    fix: add ChangeDetectionStrategy.OnPush to BravoBlotterPage

--- maia-showcase/maia-showcase-ui/src/app/pages/bravo-blotter/bravo-blotter-page.ts
+++ maia-showcase/maia-showcase-ui/src/app/pages/bravo-blotter/bravo-blotter-page.ts
@@ -1,10 +1,11 @@
-import {Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {PageLayoutComponent} from '@app/components/page-layout/page-layout.component';
 import {
     BravoCrudTableComponent
 } from '@app/gen-components/org/maiaframework/showcase/join/bravo-crud-table.component';
 
 @Component({
+    changeDetection: ChangeDetectionStrategy.OnPush,
     imports: [
         PageLayoutComponent,
         BravoCrudTableComponent
