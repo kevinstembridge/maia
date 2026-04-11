bb048035b19c1dd6a2cf946ac12c8a3d4b22ce1c
kevins-mahana <kevin.stembridge@mahanaroad.com>
2026-04-11 13:56:37 +0100

    WIP on many-to-many in AgGrid tables.

--- maia-gen/maia-gen-spec/src/main/kotlin/org/maiaframework/gen/spec/definition/RowMapperFieldDef.kt
+++ maia-gen/maia-gen-spec/src/main/kotlin/org/maiaframework/gen/spec/definition/RowMapperFieldDef.kt
@@ -2,6 +2,7 @@ package org.maiaframework.gen.spec.definition
 
 import org.maiaframework.gen.spec.definition.lang.Nullability
 
+
 data class RowMapperFieldDef(
     val entityFieldDef: EntityFieldDef,
     val nullability: Nullability,
