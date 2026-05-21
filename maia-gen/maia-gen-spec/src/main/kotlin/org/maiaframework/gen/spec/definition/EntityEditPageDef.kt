78918748e0abf19af524194634d88caeecf6bc10
kevins-mahana <kevin.stembridge@mahanaroad.com>
2026-05-21 18:35:46 +0100

    dev

--- maia-gen/maia-gen-spec/src/main/kotlin/org/maiaframework/gen/spec/definition/EntityEditPageDef.kt
+++ maia-gen/maia-gen-spec/src/main/kotlin/org/maiaframework/gen/spec/definition/EntityEditPageDef.kt
@@ -6,24 +6,31 @@ class EntityEditPageDef(
     val authority: AuthorityDef?
 ) {
 
+
     private val entityEditBaseName = entityDef.entityBaseName.withSuffix("EntityEdit")
+
 
     val editFormAngularComponentNames = AngularComponentNames(
         entityDef.packageName,
         entityEditBaseName.withSuffix("Form").value
     )
 
+
     val editFormPageAngularComponentNames = AngularComponentNames(
         entityDef.packageName,
         entityEditBaseName.withSuffix("Page").value
     )
 
+
     val dataPageId = "${entityDef.entityBaseName.toSnakeCase()}_edit"
 
+
     val viewPageUrl: String get() = entityDef.viewEntityUrl
+
 
     val updateApiDef: EntityUpdateApiDef
         get() = entityDef.entityCrudApiDef?.updateApiDef
             ?: error("Entity ${entityDef.entityBaseName} has no updateApiDef — cannot create EntityEditPageDef")
+
 
 }
