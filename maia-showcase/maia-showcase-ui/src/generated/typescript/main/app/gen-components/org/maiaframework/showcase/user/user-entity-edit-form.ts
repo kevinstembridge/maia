f6e01943add07ae521ee78c7ff0718982b60ad7d
kevins-mahana <kevin.stembridge@mahanaroad.com>
2026-05-24 00:56:39 +0100

    dev

--- maia-showcase/maia-showcase-ui/src/generated/typescript/main/app/gen-components/org/maiaframework/showcase/user/user-entity-edit-form.ts
+++ maia-showcase/maia-showcase-ui/src/generated/typescript/main/app/gen-components/org/maiaframework/showcase/user/user-entity-edit-form.ts
@@ -52,6 +52,9 @@ export class UserEntityEditForm implements OnInit {
     formGroup: FormGroup;
 
 
+    protected readonly AuthoritySelectOptions = AuthoritySelectOptions;
+
+
     private readonly formService = inject(UserCrudService);
 
 
@@ -134,7 +137,5 @@ export class UserEntityEditForm implements OnInit {
     onCancelClicked(): void {
         this.onCancel.emit();
     }
-
-
 }
 
