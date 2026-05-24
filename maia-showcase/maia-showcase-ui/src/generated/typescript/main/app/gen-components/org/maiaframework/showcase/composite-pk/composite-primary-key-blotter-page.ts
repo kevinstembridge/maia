2313d27abea34e0c5cf6d960a992725617d92ec1
kevins-mahana <kevin.stembridge@mahanaroad.com>
2026-05-24 23:22:04 +0100

    dev

--- maia-showcase/maia-showcase-ui/src/generated/typescript/main/app/gen-components/org/maiaframework/showcase/composite-pk/composite-primary-key-blotter-page.ts
+++ maia-showcase/maia-showcase-ui/src/generated/typescript/main/app/gen-components/org/maiaframework/showcase/composite-pk/composite-primary-key-blotter-page.ts
@@ -3,14 +3,14 @@
 
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {PageLayout} from '@app/components/page-layout/page-layout';
-import {CompositePrimaryKeyCrudBlotterComponent} from '@app/gen-components/org/maiaframework/showcase/composite-pk/composite-primary-key-crud-blotter';
+import {CompositePrimaryKeyBlotter} from '@app/gen-components/org/maiaframework/showcase/composite-pk/composite-primary-key-blotter';
 
 
 @Component({
     changeDetection: ChangeDetectionStrategy.OnPush,
     imports: [
         PageLayout,
-        CompositePrimaryKeyCrudBlotterComponent
+        CompositePrimaryKeyBlotter
     ],
     selector: 'app-composite-primary-key-blotter-page',
     templateUrl: './composite-primary-key-blotter-page.html'
