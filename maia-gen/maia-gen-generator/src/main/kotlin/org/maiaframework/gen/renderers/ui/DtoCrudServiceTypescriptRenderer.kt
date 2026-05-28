156929a45e81fa63131d7aa24067cd1eb7e0552d
kevins-mahana <kevin.stembridge@mahanaroad.com>
2026-05-28 23:41:50 +0100

    dev

--- maia-gen/maia-gen-generator/src/main/kotlin/org/maiaframework/gen/renderers/ui/DtoCrudServiceTypescriptRenderer.kt
+++ maia-gen/maia-gen-generator/src/main/kotlin/org/maiaframework/gen/renderers/ui/DtoCrudServiceTypescriptRenderer.kt
@@ -11,26 +11,9 @@ class DtoCrudServiceTypescriptRenderer(
 
     init {
 
-        addImport("@angular/core", "inject")
-        addImport("@angular/core", "Injectable")
-        addImport("@angular/common/http", "HttpClient")
-        addImport("@angular/common/http", "HttpHeaders")
-        addImport("rxjs", "Observable")
-
-        this.entityCrudApiDef.createApiDef?.let { addImport(it.requestDtoDef.typescriptImport) }
-        this.entityCrudApiDef.updateApiDef?.let { addImport(it.requestDtoDef.typescriptImport) }
-        this.entityCrudApiDef.entityDef.fetchForEditDtoDef?.let { addImport(it.typescriptImport) }
-
-        val indexDefsWithExistsEndpoint = this.entityCrudApiDef.entityDef.uniqueIndexDefs.filter { it.withExistsEndpoint }
-        if (indexDefsWithExistsEndpoint.isNotEmpty()) {
-            indexDefsWithExistsEndpoint.forEach { entityIndexDef ->
-                addImport(entityIndexDef.asyncValidator.asyncValidationDtoTypescriptImport)
-            }
-            addImport("@maia/maia-ui", "FormValidationResponseDto")
-        }
+        `add imports`()
 
     }
-
 
     override fun renderedFilePath(): String {
 
@@ -58,21 +41,48 @@ class DtoCrudServiceTypescriptRenderer(
             |
             |""".trimMargin())
 
+        `render the create function`()
+
+        `render the existsBy function for unique indexes`()
+
+        `render the edit function`()
+
+        `render the fetchForEdit function`()
+
+        `render the delete function`()
+
+        append("""
+            |
+            |
+            |}
+            |""".trimMargin())
+
+    }
+
+
+    private fun `render the create function`() {
+
         this.entityCrudApiDef.createApiDef?.let { apiDef ->
 
             val createRequestDtoDef = apiDef.requestDtoDef
 
             append("""
-                |    public create(requestDto: ${createRequestDtoDef.uqcn}): Observable<void> {
+                |    public create(requestDto: ${createRequestDtoDef.uqcn}): Observable<EntityCreatedResponseDto> {
                 |
-                |        return this.http.post<void>(
+                |        return this.http.post<EntityCreatedResponseDto>(
                 |                '${apiDef.endpointUrl}',
                 |                requestDto,
                 |                this.httpOptions);
                 |
                 |    }
-                |""".trimMargin())
+                |""".trimMargin()
+            )
         }
+
+    }
+
+
+    private fun `render the existsBy function for unique indexes`() {
 
         this.entityCrudApiDef.entityDef.uniqueIndexDefs.filter { it.withExistsEndpoint }.forEach { entityIndexDef ->
 
@@ -84,9 +94,15 @@ class DtoCrudServiceTypescriptRenderer(
                 |        return this.http.post<FormValidationResponseDto>('${entityIndexDef.existsByUrl}', requestBody, this.httpOptions);
                 |
                 |    }
-                |""".trimMargin())
+                |""".trimMargin()
+            )
 
         }
+
+    }
+
+
+    private fun `render the edit function`() {
 
         this.entityCrudApiDef.updateApiDef?.let { apiDef ->
 
@@ -103,9 +119,15 @@ class DtoCrudServiceTypescriptRenderer(
                 |                this.httpOptions);
                 |
                 |    }
-                |""".trimMargin())
+                |""".trimMargin()
+            )
 
         }
+
+    }
+
+
+    private fun `render the fetchForEdit function`() {
 
         this.entityCrudApiDef.entityDef.fetchForEditDtoDef?.let { fetchForEditDtoDef ->
 
@@ -117,9 +139,15 @@ class DtoCrudServiceTypescriptRenderer(
                 |        return this.http.get<${fetchForEditDtoDef.uqcn}>('${fetchForEditDtoDef.endpointUrl}/' + id, this.httpOptions);
                 |
                 |    }
-                |""".trimMargin())
+                |""".trimMargin()
+            )
 
         }
+
+    }
+
+
+    private fun `render the delete function`() {
 
         this.entityCrudApiDef.deleteApiDef?.let { apiDef ->
 
@@ -127,7 +155,8 @@ class DtoCrudServiceTypescriptRenderer(
             if (entityDef.hasCompositePrimaryKey) {
 
                 val pkType = entityDef.primaryKeyClassFields.joinToString(", ") { field ->
-                    val tsType = if (field.fieldType is IntFieldType || field.fieldType is LongFieldType) "number" else "string"
+                    val tsType =
+                        if (field.fieldType is IntFieldType || field.fieldType is LongFieldType) "number" else "string"
                     "${field.classFieldName.value}: $tsType"
                 }
                 val deleteBaseUrl = apiDef.endpointUrl.trimEnd('/')
@@ -143,7 +172,8 @@ class DtoCrudServiceTypescriptRenderer(
                     |        });
                     |
                     |    }
-                    |""".trimMargin())
+                    |""".trimMargin()
+                )
 
             } else {
 
@@ -155,16 +185,39 @@ class DtoCrudServiceTypescriptRenderer(
                     |        return this.http.delete('${apiDef.endpointUrl}' + id, this.httpOptions);
                     |
                     |    }
-                    |""".trimMargin())
+                    |""".trimMargin()
+                )
 
             }
+
         }
 
-        append("""
-            |
-            |
-            |}
-            |""".trimMargin())
+    }
+
+
+    private fun `add imports`() {
+
+        addImport("@angular/core", "inject")
+        addImport("@angular/core", "Injectable")
+        addImport("@angular/common/http", "HttpClient")
+        addImport("@angular/common/http", "HttpHeaders")
+        addImport("rxjs", "Observable")
+
+        this.entityCrudApiDef.createApiDef?.let {
+            addImport(it.requestDtoDef.typescriptImport)
+            addImport("@maia/maia-ui", "EntityCreatedResponseDto")
+        }
+        this.entityCrudApiDef.updateApiDef?.let { addImport(it.requestDtoDef.typescriptImport) }
+        this.entityCrudApiDef.entityDef.fetchForEditDtoDef?.let { addImport(it.typescriptImport) }
+
+        val indexDefsWithExistsEndpoint =
+            this.entityCrudApiDef.entityDef.uniqueIndexDefs.filter { it.withExistsEndpoint }
+        if (indexDefsWithExistsEndpoint.isNotEmpty()) {
+            indexDefsWithExistsEndpoint.forEach { entityIndexDef ->
+                addImport(entityIndexDef.asyncValidator.asyncValidationDtoTypescriptImport)
+            }
+            addImport("@maia/maia-ui", "FormValidationResponseDto")
+        }
 
     }
 
