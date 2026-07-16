4afac3acbb135ab129c6fbdd2e8590fe2d5216ce
kevins-mahana <kevin.stembridge@mahanaroad.com>
2026-07-16 07:28:12 +0100

    dev

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,8 +12,8 @@ repositories {
 
 dependencies {
 
-    implementation("org.jetbrains.kotlin:kotlin-allopen:2.3.20")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.20")
-    implementation("org.springframework.boot:spring-boot-gradle-plugin:4.0.5")
+    implementation("org.jetbrains.kotlin:kotlin-allopen:2.3.21")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.21")
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:4.1.0")
 
 }
