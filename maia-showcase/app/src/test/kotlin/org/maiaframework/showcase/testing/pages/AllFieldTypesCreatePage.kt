47790ee7df36befa543dcac246c4c28147fe1ec7
kevins-mahana <kevin.stembridge@mahanaroad.com>
2026-07-19 10:40:38 +0100

    dev

--- maia-showcase/app/src/test/kotlin/org/maiaframework/showcase/testing/pages/AllFieldTypesCreatePage.kt
+++ maia-showcase/app/src/test/kotlin/org/maiaframework/showcase/testing/pages/AllFieldTypesCreatePage.kt
@@ -1,7 +1,9 @@
 package org.maiaframework.showcase.testing.pages
 
+import com.microsoft.playwright.Locator
 import com.microsoft.playwright.Page
 import com.microsoft.playwright.options.AriaRole
+import com.microsoft.playwright.options.WaitForSelectorState
 import org.maiaframework.webtesting.AbstractPage
 import org.maiaframework.webtesting.UrlHelper
 
@@ -58,13 +60,18 @@ class AllFieldTypesCreatePage(
         page.locator("input[name='someProvidedStringType']").fill(someProvidedStringType)
         page.locator("mat-select[formControlName='someEnum']").click()
         page.getByRole(AriaRole.OPTION, Page.GetByRoleOptions().setName(someEnum)).click()
-        Thread.sleep(1000)
+        page.mouse().move(0.0, 0.0)
+        page.evaluate("document.querySelector('.cdk-overlay-transparent-backdrop')?.click()")
+        page.locator("mat-option").first().waitFor(
+            Locator.WaitForOptions().setState(WaitForSelectorState.HIDDEN)
+        )
 
     }
 
 
     fun clickSubmitButton() {
-        page.getByRole(AriaRole.BUTTON, Page.GetByRoleOptions().setName("Submit")).click()
+        page.getByRole(AriaRole.BUTTON, Page.GetByRoleOptions().setName("Submit"))
+            .click(Locator.ClickOptions().setForce(true))
     }
 
 
