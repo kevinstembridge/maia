90ef73e081e6d02cfa9d2ff83678fe3c5208cd5e
kevins-mahana <kevin.stembridge@mahanaroad.com>
2026-08-30 12:30:28 +0100

    dev

--- libs/maia-file-storage-parent/maia-file-storage-autoconfigure/build.gradle.kts
+++ libs/maia-file-storage-parent/maia-file-storage-autoconfigure/build.gradle.kts
@@ -8,6 +8,7 @@ dependencies {
 
     api(project(":libs:maia-file-storage-parent:maia-file-storage"))
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-flyway")
 
     annotationProcessor("org.springframework.boot:spring-boot-autoconfigure-processor:3.5.6")
 
--- libs/maia-file-storage-parent/maia-file-storage-autoconfigure/src/main/kotlin/org/maiaframework/storage/MaiaFileStorageAutoConfiguration.kt
+++ libs/maia-file-storage-parent/maia-file-storage-autoconfigure/src/main/kotlin/org/maiaframework/storage/MaiaFileStorageAutoConfiguration.kt
@@ -1,13 +1,27 @@
 package org.maiaframework.storage
 
+import org.maiaframework.jdbc.JdbcOps
+import org.maiaframework.storage.hazelcast.HazelcastConfig
 import org.springframework.boot.autoconfigure.AutoConfiguration
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.flyway.autoconfigure.FlywayAutoConfiguration
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration
+import org.springframework.boot.jdbc.autoconfigure.JdbcTemplateAutoConfiguration
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
+import javax.sql.DataSource
 
-@AutoConfiguration
+@AutoConfiguration(
+    after = [
+        DataSourceAutoConfiguration::class,
+        JdbcTemplateAutoConfiguration::class,
+        FlywayAutoConfiguration::class
+    ]
+)
 @ConditionalOnClass(FileStorage::class)
 @EnableConfigurationProperties(MaiaFileStorageProperties::class)
 class MaiaFileStorageAutoConfiguration(private val properties: MaiaFileStorageProperties) {
@@ -22,6 +36,34 @@ class MaiaFileStorageAutoConfiguration(private val properties: MaiaFileStoragePr
             ?: throw IllegalStateException("Expecting maia.file-storage.local.base-dir to be configured.")
 
         return LocalFileStorage(baseDir, fileStorageEntryDao)
+
+    }
+
+
+    @Bean
+    @ConditionalOnMissingBean(HazelcastConfig::class)
+    fun fileStorageHazelcastConfig(): HazelcastConfig {
+
+        return HazelcastConfig()
+
+    }
+
+
+    @Configuration
+    @ConditionalOnMissingBean(FileStorageEntryDao::class)
+    class FileStorageEntryDaoConfiguration {
+
+
+        @Bean
+        @ConditionalOnMissingBean
+        fun fileStorageEntryDao(dataSource: DataSource): FileStorageEntryDao {
+
+            val jdbcOps = JdbcOps(NamedParameterJdbcTemplate(dataSource))
+
+            return FileStorageEntryDao(FileStorageEntryEntityFieldConverter(), jdbcOps)
+
+        }
+
 
     }
 
