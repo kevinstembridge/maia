fc0c9046d63ec3d3a8940f35c432315024778889
kevins-mahana <kevin.stembridge@mahanaroad.com>
2026-09-24 21:53:46 +0100

    dev

--- libs/maia-elasticsearch-parent/maia-elasticsearch/src/main/kotlin/org/maiaframework/elasticsearch/index/EsIndexStateDto.kt
+++ libs/maia-elasticsearch-parent/maia-elasticsearch/src/main/kotlin/org/maiaframework/elasticsearch/index/EsIndexStateDto.kt
@@ -1,10 +1,13 @@
 package org.maiaframework.elasticsearch.index
 
 data class EsIndexStateDto(
-        val indexName: EsIndexName,
-        val summary: EsIndexSummaryDto?,
-        val health: EsIndexHealthDto?) {
+    val indexName: EsIndexName,
+    val summary: EsIndexSummaryDto?,
+    val health: EsIndexHealthDto?
+) {
 
+    
     val indexExists = this.health != null
+
 
 }
