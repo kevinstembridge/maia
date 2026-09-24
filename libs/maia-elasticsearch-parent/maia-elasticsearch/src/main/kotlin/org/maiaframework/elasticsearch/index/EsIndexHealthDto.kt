2a19d6f2325371eba14d783b61a3acbee6d3522d
kevins-mahana <kevin.stembridge@mahanaroad.com>
2026-09-24 21:55:52 +0100

    dev

--- libs/maia-elasticsearch-parent/maia-elasticsearch/src/main/kotlin/org/maiaframework/elasticsearch/index/EsIndexHealthDto.kt
+++ libs/maia-elasticsearch-parent/maia-elasticsearch/src/main/kotlin/org/maiaframework/elasticsearch/index/EsIndexHealthDto.kt
@@ -1,5 +1,7 @@
 package org.maiaframework.elasticsearch.index
 
+
 data class EsIndexHealthDto(
-        val indexName: EsIndexName,
-        val status: String)
+    val indexName: EsIndexName,
+    val status: String
+)
